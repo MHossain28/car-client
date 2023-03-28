161c14d9d7dcc13d62cc319b212cfbb8ff553325
MHossain <mhh52728@gmail.com>
2023-03-28 23:25:03 +0600

    banner update

---
+++ src/Pages/Home/Home/FristBanner/FristBanner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./FristBanner.css";
+import imgdd from "../../../../assets/images/banner/4.jpg";
+
+const FristBanner = () => {
+  return (
+    <div className="text-white">
+      <div
+        className="banner"
+        style={{
+          backgroundImage: `url("https://img.freepik.com/free-photo/mechanic-holding-pair-wrenches-repair-garage_146671-19612.jpg?size=626&ext=jpg&ga=GA1.2.1847957643.1680023391&semt=ais")`,
+        }}
+      >
+        <div className="text-center pt-16">
+          <h1 className="text-4xl text-info">Car Repair & Services</h1>
+          <img className="img" height={280} width={280} src={imgdd} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FristBanner;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Services from "../../Services/Services";
+import FristBanner from "./FristBanner/FristBanner";
 
 const Home = () => {
   return (
     <div>
+      <FristBanner></FristBanner>
       <Services></Services>
     </div>
   );
